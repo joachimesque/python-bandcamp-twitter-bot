57ed35fe4000ded8f5f594cd3e4e822398fe64ac
Joachim <joachim.robert@protonmail.com>
2020-11-15 13:06:50 +0100

    Accept args (#2)

--- files_handler.py
+++ files_handler.py
@@ -5,24 +5,29 @@ import json
 import linecache
 import sys
 
-def get_tag_line():
-  current_index = linecache.getline('current_index.txt',1)
+def get_tag_line(index_path, tag_list_path):
+  current_index = linecache.getline(index_path, 1)
 
   try:
     current_index = int(current_index)
+    tag_line = linecache.getline(tag_list_path, current_index).strip()
+
+    if tag_line == '':
+      raise ValueError()
   except:
     current_index = 1
+    tag_line = linecache.getline(tag_list_path, 1).strip()
 
-  tag_line = linecache.getline('tag_list.txt', current_index).strip()
+  try:
+    tag_line = json.loads(tag_line)
+  except:
+    print("Error: there was a problem getting the tag line.")
+    sys.exit()
 
-  if tag_line == '':
-    current_index = 1
-    tag_line = linecache.getline('tag_list.txt', 1).strip()
+  return [current_index, tag_line]
 
-  return [current_index, json.loads(tag_line)]
-
-def write_index(current_index):
-  with open('current_index.txt', 'w') as index_file:
-    index_file.write(str(current_index + 1))
+def write_index(index, index_path):
+  with open(index_path, 'w') as index_file:
+    index_file.write(str(index + 1))
 
   index_file.close()
--- main.py
+++ main.py
@@ -5,22 +5,51 @@ import files_handler
 import bandcamp
 import sentences
 import twitter_utils
+import argparse
 
-file_contents = files_handler.get_tag_line()
+# Parse arguments
+
+parser = argparse.ArgumentParser("Bandcamp Twitter tool")
+parser.add_argument("index_path",
+                    help="Path to your index file",
+                    default="index_path=current_index.txt",
+                    nargs="?"
+                    )
+parser.add_argument("tag_list_path",
+                    help="Path to your tag list file",
+                    default="tag_list_path=tag_list.txt",
+                    nargs="?"
+                    )
+args = parser.parse_args()
+index_path = args.index_path.split("=")[1]
+tag_list_path = args.tag_list_path.split("=")[1]
+
+# Get current index and tag
+
+file_contents = files_handler.get_tag_line(index_path = index_path,
+                                           tag_list_path = tag_list_path)
 current_index = file_contents[0]
 tag = file_contents[1]
 tag_value = tag['value']
 tag_name = tag['name']
 
+# Get album info from tag
+
 all_albums = bandcamp.get_albums_from_tag(tag = tag_value)
-
 album = all_albums['items'][0]
-album_title = album['title']
-album_artist = album['artist']
-album_url = album['tralbum_url']
 
-sentence = sentences.get_sentence(tag_name, album_title, album_artist, album_url)
+# Get sentence
+
+sentence = sentences.get_sentence(tag_name = tag_name,
+                                  album_title = album['title'],
+                                  album_artist = album['artist'],
+                                  album_url = album['tralbum_url'])
+
+# Output sentence
 
 twitter_utils.tweet(sentence)
 
-files_handler.write_index(current_index)
+# Write new index
+
+files_handler.write_index(index= current_index,
+                          index_path = index_path)
