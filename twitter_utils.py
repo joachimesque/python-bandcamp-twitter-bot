313e1bdb529a85da902e7c8fb1c39114eb8f0bfa
Joachim Robert <joachim.robert@protonmail.com>
2020-11-15 11:42:36 +0100

    Update twitter_utils.py

--- twitter_utils.py
+++ twitter_utils.py
@@ -4,12 +4,16 @@
 import os
 import tweepy
 
-consumer_key = os.environ['CONSUMER_KEY']
-consumer_secret = os.environ['CONSUMER_SECRET']
-access_token = os.environ['ACCESS_TOKEN']
-access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-
 def tweet(message):
+  # If no config is found, bypass Twitter and output the tweet
+  try:
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+    access_token = os.environ['ACCESS_TOKEN']
+    access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+  except:
+    return print(message)
+
   # Authenticate to Twitter
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
